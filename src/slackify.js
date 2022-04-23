feef812cbfbc233fcd826e0924a6c22f3cc66299
Joseph Rendon <javraindawn@gmail.com>
2022-04-23 10:47:26 +0800

    fails on safari fix maybe

--- src/slackify.js
+++ src/slackify.js
@@ -1,10 +1,10 @@
-const defaultHandlers = require('mdast-util-to-markdown/lib/handle');
-const phrasing = require('mdast-util-to-markdown/lib/util/container-phrasing');
+const defaultHandlers = require("mdast-util-to-markdown/lib/handle");
+const phrasing = require("mdast-util-to-markdown/lib/util/container-phrasing");
 
-const { wrap, isURL, isPotentiallyEncoded } = require('./utils');
+const { wrap, isURL, isPotentiallyEncoded } = require("./utils");
 
 // fixes slack in-word formatting (e.g. hel*l*o)
-const zeroWidthSpace = String.fromCharCode(0x200B);
+const zeroWidthSpace = String.fromCharCode(0x200b);
 
 /**
  * Creates custom `mdast-util-to-markdown` handlers that tailor the output for
@@ -15,12 +15,12 @@ const zeroWidthSpace = String.fromCharCode(0x200B);
  *
  * @returns {import('mdast-util-to-markdown').Handlers}
  */
-const createHandlers = definitions => ({
+const createHandlers = (definitions) => ({
   heading: (node, _parent, context) => {
     // make headers to be just *strong*
-    const marker = '*';
+    const marker = "*";
 
-    const exit = context.enter('heading');
+    const exit = context.enter("heading");
     const value = phrasing(node, context, { before: marker, after: marker });
     exit();
 
@@ -28,9 +28,9 @@ const createHandlers = definitions => ({
   },
 
   strong: (node, _parent, context) => {
-    const marker = '*';
+    const marker = "*";
 
-    const exit = context.enter('strong');
+    const exit = context.enter("strong");
     const value = phrasing(node, context, { before: marker, after: marker });
     exit();
 
@@ -38,9 +38,9 @@ const createHandlers = definitions => ({
   },
 
   delete(node, _parent, context) {
-    const marker = '~';
+    const marker = "~";
 
-    const exit = context.enter('delete');
+    const exit = context.enter("delete");
     const value = phrasing(node, context, { before: marker, after: marker });
     exit();
 
@@ -48,32 +48,30 @@ const createHandlers = definitions => ({
   },
 
   emphasis: (node, _parent, context) => {
-    const marker = '_';
+    const marker = "_";
 
-    const exit = context.enter('emphasis');
+    const exit = context.enter("emphasis");
     const value = phrasing(node, context, { before: marker, after: marker });
     exit();
 
     return wrap(value, zeroWidthSpace, marker);
   },
 
-  listItem: (...args) => defaultHandlers
-    .listItem(...args)
-    .replace(/^\*/, '•'),
+  listItem: (...args) => defaultHandlers.listItem(...args).replace(/^\*/, "•"),
 
   code(node, _parent, context) {
-    const exit = context.enter('code');
+    const exit = context.enter("code");
     // delete language prefix for deprecated markdown formatters (old Bitbucket Editor)
-    const content = node.value.replace(/^#![a-z]+\n/, ''); // ```\n#!javascript\ncode block\n```
+    const content = node.value.replace(/^#![a-z]+\n/, ""); // ```\n#!javascript\ncode block\n```
     exit();
 
-    return wrap(content, '```', '\n');
+    return wrap(content, "```", "\n");
   },
 
   link: (node, _parent, context) => {
-    const exit = context.enter('link');
-    const text = phrasing(node, context, { before: '|', after: '>' })
-      || node.title;
+    const exit = context.enter("link");
+    const text =
+      phrasing(node, context, { before: "|", after: ">" }) || node.title;
     const url = isPotentiallyEncoded(node.url) ? node.url : encodeURI(node.url);
     exit();
 
@@ -83,10 +81,11 @@ const createHandlers = definitions => ({
   },
 
   linkReference: (node, _parent, context) => {
-    const exit = context.enter('linkReference');
+    const exit = context.enter("linkReference");
     const definition = definitions[node.identifier];
-    const text = phrasing(node, context, { before: '|', after: '>' })
-      || (definition ? definition.title : null);
+    const text =
+      phrasing(node, context, { before: "|", after: ">" }) ||
+      (definition ? definition.title : null);
     exit();
 
     if (!definition || !isURL(definition.url)) return text;
@@ -95,7 +94,7 @@ const createHandlers = definitions => ({
   },
 
   image: (node, _parent, context) => {
-    const exit = context.enter('image');
+    const exit = context.enter("image");
     const text = node.alt || node.title;
     const url = encodeURI(node.url);
     exit();
@@ -106,10 +105,9 @@ const createHandlers = definitions => ({
   },
 
   imageReference: (node, _parent, context) => {
-    const exit = context.enter('imageReference');
+    const exit = context.enter("imageReference");
     const definition = definitions[node.identifier];
-    const text = node.alt
-      || (definition ? definition.title : null);
+    const text = node.alt || (definition ? definition.title : null);
     exit();
 
     if (!definition || !isURL(definition.url)) return text;
@@ -118,12 +116,12 @@ const createHandlers = definitions => ({
   },
 
   text: (node, _parent, context) => {
-    const exit = context.enter('text');
+    const exit = context.enter("text");
     // https://api.slack.com/reference/surfaces/formatting#escaping
     const text = node.value
-      .replace(/&/g, '&amp;')
-      .replace(/<(?!@)/g, '&lt;')
-      .replace(/(?<!@[A-Z0-9]+)>/g, '&gt;');
+      .replace(/&/g, "&amp;")
+      .replace(/<(?!@)/g, "&lt;")
+      .replace(/>/g, "&gt;");
     exit();
 
     // Do we need more escaping like the default handler uses?
@@ -142,8 +140,8 @@ const createHandlers = definitions => ({
  *
  * @returns {import('remark-stringify').RemarkStringifyOptions}
  */
-const createOptions = definitions => ({
-  bullet: '*',
+const createOptions = (definitions) => ({
+  bullet: "*",
   handlers: createHandlers(definitions),
 });
 
